ce56dd8494c333d24111ae5ea44fb16625109d75
Ebonkuab <idaku6@yahoo.com>
2016-02-04 02:09:12 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieAppRottenMangoes::Application.config.secret_key_base = '97d64f681f92c34cecf1307628eeb48721af1085edd394965b5bb58c42f079c97839bb2878fa91d96bb27b5f62c3667fbeed642cdf935fe89f862bf402703d89'
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+MovieAppRottenMangoes::Application.routes.draw do
+  get "reviews/new"
+  get "reviews/create"
+  get "sessions/new"
+  get "sessions/create"
+  get "users/new"
+  get "users/create"
+  # get "movies/index"
+  # get "movies/show"
+  # get "movies/new"
+  # get "movies/edit"
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end 
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]  
+   root to: 'movies#index'
+end
